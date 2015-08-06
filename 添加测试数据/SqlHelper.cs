77346a97e914586b7af20c9814709585662c73ca
houlongchao <hlc2015@qq.com>
2015-08-06 17:31:36 +0800

    批量添加Book表数据
    为了添加测试数据方便，
    用了一个WinForm小程序批量添加测试数据，
    目前只写了批量添加Book表的数据

---
+++ 添加测试数据/SqlHelper.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace System.Data
+{
+    public static class SqlHelper
+    {
+        //连接字符串
+        private static readonly string connStr = System.Configuration.ConfigurationManager.ConnectionStrings["connStr"].ConnectionString;
+
+        //封装常用方法
+        
+        public static int ExecuteNonQuery(string sql,CommandType cmdType,params SqlParameter[] pms)
+        {
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                using (SqlCommand cmd  = new SqlCommand(sql,conn))
+                {
+                    cmd.CommandType = cmdType;
+                    if (pms!=null)
+                    {
+                        cmd.Parameters.AddRange(pms);  
+                    }
+                    conn.Open();
+                    return cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public static object ExecuteScalar(string sql,CommandType cmdType, params SqlParameter[] pms)
+        {
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                using (SqlCommand cmd = new SqlCommand(sql,conn))
+                {
+                    cmd.CommandType = cmdType;
+                    if (pms!=null)
+                    {
+                        cmd.Parameters.AddRange(pms);
+                    }
+                    conn.Open();
+                    return cmd.ExecuteScalar();
+                }
+            }
+        }
+
+        
+        public static SqlDataReader ExecuteReader(string sql, CommandType cmdType , params SqlParameter[] pms)
+        {
+            SqlConnection conn = new SqlConnection(connStr);
+                using (SqlCommand cmd = new SqlCommand(sql, conn))
+                {
+                    cmd.CommandType = cmdType;
+                    if (pms!=null)
+                    {
+                        cmd.Parameters.AddRange(pms);
+                    }
+                    try
+                    {
+                        conn.Open();
+                        return cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                    }
+                    catch
+                    {
+                        conn.Close();
+                        conn.Dispose();
+                        throw;
+                    }
+                }
+        }
+
+        //返回DataTable
+        public static DataTable ExecuteDatatable(string sql, CommandType cmdtype, params SqlParameter[] pms)
+        {
+            DataTable dt = new DataTable();
+            using (SqlDataAdapter adapter = new SqlDataAdapter(sql,connStr))
+            {
+                adapter.SelectCommand.CommandType = cmdtype;
+                if (pms!=null)
+                {
+                    adapter.SelectCommand.Parameters.AddRange(pms);
+                }
+                adapter.Fill(dt);
+                return dt;
+            }
+        }
+
+    }
+}
